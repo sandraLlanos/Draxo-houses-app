57404dee81530b6c9354a846ea88a94e3bea5b99
sandra llanos <sandra.llanos@inxeption.com>
2021-04-22 10:40:55 -0500

    houses app created

---
+++ src/HousesApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HouseGrid } from './components/HouseGrid';
+
+export const HousesApp = () => {
+
+    return (
+        <>
+            <h1><span>Draxo</span> Houses APP</h1>
+            <HouseGrid />
+        </>
+    )
+}
